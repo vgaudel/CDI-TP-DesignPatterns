2df9a616bf8642160a89b3e186ffc929e099e05e
Gaudel Vincent <vgaudel@gmail.com>
2021-07-05 10:14:05 +0200

    Adding folders

---
+++ singleton.java
@@ -0,0 +1,17 @@
+class Singleton {
+
+private static Singleton _instance;
+
+private  Singleton (){
+
+}
+
+public static Singleton getInstance(){
+    if (Singleton._instance == null) {
+        Singleton._instance = new Singleton();
+    }
+    
+    return Singleton._instance;
+}
+
+}
